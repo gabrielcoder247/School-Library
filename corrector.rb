199dc8e06b54a47a1c54a0e3fa4f6c96ef28c6b7
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-01-17 17:38:57 +0100

    Fix linter errors

--- corrector.rb
+++ corrector.rb
@@ -1,9 +1,9 @@
 class Corrector
-    def correct_name(name)
-      if name.length > 10
-        name[0...10].capitalize
-      else
-        name.capitalize
-      end
+  def correct_name(name)
+    if name.length > 10
+      name[0...10].capitalize
+    else
+      name.capitalize
     end
   end
+end
