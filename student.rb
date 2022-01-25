b87f9f7fa2126d091efd416d213425077cb035ee
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-01-25 22:59:27 +0100

    Persist data

--- student.rb
+++ student.rb
@@ -1,13 +1,20 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'unkown', parent_permission: true)
-    super(age, name)
+  attr_reader :classroom
+  attr_accessor :id
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission = 'true')
+    super(age, name, parent_permission)
     @classroom = classroom
-    @parent_permission = parent_permission
   end
 
-  def play_hockey
+  def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
